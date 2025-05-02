661de933f2e3a74c3597a9b519727df8c8414ea7
Eduardo <ducu_yusuke@hotmail.com>
2025-05-02 13:43:33 -0300

    pundit implementado e testado

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,35 +1,43 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  #TODO: cleanup this callback
+  after_action :verify_policy_scoped, only: :index
 
   def new
     @task = Task.new
+    authorize @task
   end
 
   # TODO: add unprocessable entity
   def create
     @task = Task.new(task_params)
     @task.user = current_user
+    authorize @task
     @task.save
     redirect_to task_path(@task)
   end
 
   def index
-    @tasks = Task.all
+    @tasks = policy_scope(Task)
   end
 
   def show
+    authorize @task
   end
 
   def edit
+    authorize @task
   end
 
   # TODO: add unprocessable entity
   def update
+    authorize @task
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
+    authorize @task
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -40,7 +48,8 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  # TODO: check permited properties after later migrations
   def task_params
-    params.require(:task).permit(:name, :done, :user_id)
+    params.require(:task).permit(:name, :done)
   end
 end
