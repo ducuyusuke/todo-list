9cd6b912367c2ea3ce01f596e55808de42ac1401
Eduardo <ducu_yusuke@hotmail.com>
2025-05-05 17:15:50 -0300

    added position logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,10 +18,10 @@ class TasksController < ApplicationController
   end
 
   def index
-    @tasks = policy_scope(Task).where(done: false)
+    @tasks = policy_scope(Task).where(done: false).order(position: :asc)
     if params[:query].present?
       # TODO: if I remove the author field, remove here as well
-      @tasks = policy_scope(Task).where("name LIKE :query OR author LIKE :query", query: "%#{params[:query]}%").order(done: :asc)
+      @tasks = policy_scope(Task).where("name LIKE :query OR author LIKE :query", query: "%#{params[:query]}%").order(done: :asc, position: :asc)
     end
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,18 @@
 class Task < ApplicationRecord
   belongs_to :user
+  
+  before_create :set_initial_position
 
   def mark_as_done!
     self.update(done: !done)
+  end
+
+  private
+
+  def set_initial_position
+    if self.position.nil?
+      last_position = Task.where(user: self.user).maximum(:position) || 0
+      self.position = last_position + 1
+    end
   end
 end
