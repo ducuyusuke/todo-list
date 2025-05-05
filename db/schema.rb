48cd9e89cf295897db631194abdfb3bc018dbefd
Eduardo <ducu_yusuke@hotmail.com>
2025-05-05 16:34:41 -0300

    added search and navigation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_02_165059) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_05_191401) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "done", default: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author", default: ""
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
